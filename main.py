085293edf5dd0704f077e9b1a95c160a3922cb56
Joshjt7 <Josh11joshua11@gmail.com>
2026-04-18 16:15:47 +0100

    Adding Teams

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Page config
 # ---------------------------------
 st.set_page_config(
-    page_title="World Cup Family Pool",
+    page_title="Winstons World Cup Fun",
     page_icon="⚽",
     layout="wide"
 )
